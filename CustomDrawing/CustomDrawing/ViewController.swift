7d74b96a3cf7a16e5df5cade066d1bf1bfe3bdbe
ugurcandemiir <ugurcandemiir@gmail.com>
2021-03-05 21:00:17 -0500

    Added Clear and Undo Buttons

--- CustomDrawing/CustomDrawing/ViewController.swift
+++ CustomDrawing/CustomDrawing/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
 
     let canvas = Canvas()
     
+    let undoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Undo", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 15)
+        return button
+    }()
+    let clearButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Clear", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 15)
+        return button
+    }()
+    
     override func loadView() {
         self.view = canvas
     }
@@ -20,8 +33,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         canvas.backgroundColor = .white
+        
+        let stackView = UIStackView(arrangedSubviews:[undoButton,clearButton])
 //        view.addSubview(canvas)
 //        canvas.frame = view.frame
+        view.addSubview(stackView)
+        stackView.frame = CGRect(x: 10 , y: 10, width: 200, height: 100)
     }
     
 }
